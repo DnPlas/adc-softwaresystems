d00dd87b2900bc14a14219f6dde56255990b73c5
Daniela Plascencia <daplascen@gmail.com>
2021-03-10 16:35:25 -0600

    lab3.2: adds file-cmp_test.py test cases
    Signed-off-by: Daniela Plascencia <daplascen@gmail.com>

---
+++ lab3.2/file-cpm_test.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+"""
+This program tests the filecmp.cmp() method
+"""
+
+import filecmp
+import unittest
+
+file_1 = "text_1.txt"
+file_1_clone = "text_1_clone.txt"
+file_2 = "text_2.txt"
+
+class TestFilecmpCMP(unittest.TestCase):
+
+    # (R) Right
+    def test_comparison(self):
+        self.assertTrue(filecmp.cmp(file_1, file_1_clone))
+
+    def test_comparison_false(self):
+        self.assertFalse(filecmp.cmp(file_1, file_2))
+
+    def test_contents_eq(self):
+        self.assertTrue(filecmp.cmp(file_1, file_1_clone, shallow=False))
+
+    def test_contents_diff(self):
+        self.assertFalse(filecmp.cmp(file_1, file_2, shallow=False))
+
+    def test_cache(self):
+        filecmp.clear_cache()
+        self.assertTrue(filecmp.cmp(file_1, file_1_clone, shallow=False))
+
+if __name__=='__main__':
+    unittest.main()
