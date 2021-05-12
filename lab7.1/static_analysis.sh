3fc0cb8d76b626015b136f477817c6f90c382b8a
Daniela Plascencia <daplascen@gmail.com>
2021-05-12 00:50:35 -0500

    adds PATH
    Signed-off-by: Daniela Plascencia <daplascen@gmail.com>

--- lab7.1/static_analysis.sh
+++ lab7.1/static_analysis.sh
@@ -1,6 +1,5 @@
 #! /usr/bin/env bash
 
-BIN_PATH=/home/daplascen/.local/bin
 REPO="https://github.com/DnPlas/adc-softwaresystems.git"
 REPO_SUFFIX="dnplas-adc-softwaresystems"
 CLONE="sa_$REPO_SUFFIX"
@@ -28,11 +27,11 @@ function clone_repo(){
 }
 
 function run_sa(){
-    ${BIN_PATH}/prospector > sa_logs/sa_prospector_$current_time.out
-    ${BIN_PATH}/radon cc . > sa_logs/cc_radon_$current_time.out
-    ${BIN_PATH}/radon raw . > sa_logs/raw_radon_$current_time.out
-    ${BIN_PATH}/radon mi . > sa_logs/mi_radon_$current_time.out
-    ${BIN_PATH}/radon hal . > sa_logs/hal_radon_$current_time.out
+    prospector > sa_logs/sa_prospector_$current_time.out
+    radon cc . > sa_logs/cc_radon_$current_time.out
+    radon raw . > sa_logs/raw_radon_$current_time.out
+    radon mi . > sa_logs/mi_radon_$current_time.out
+    radon hal . > sa_logs/hal_radon_$current_time.out
 }
 
 function main_on_prem(){
